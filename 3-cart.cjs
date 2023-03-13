08b04df7a447367bfe03572a805222893e7ca468
= <=>
2023-03-13 12:12:20 +0530

    3-cart.cjs
    Question 1 completed but a little not in exact formate as input

--- 3-cart.cjs
+++ 3-cart.cjs
@@ -15,9 +15,11 @@ const products = [{
     utensils: [
         {
             spoons: { quantity: 2, price: "$8" }
-        }, {
+        },
+        {
             glasses: { quantity: 1, price: "$70", type: "fragile" }
-        }, {
+        },
+        {
             cooker: { quantity: 4, price: "$900" }
         }
     ],
@@ -26,6 +28,7 @@ const products = [{
         quantity: 1,
         type: "fragile"
     }
+
 }];
 
 
@@ -43,41 +46,94 @@ NOTE: Do not change the name of this file
 
 // Question 1.
 
+function itemsPriceMoreThen$65(items) {
+
+    let productsName = Object.keys(items[0]);
+
+    let desiredProduct = productsName.reduce((productsAbove65, product, index, productsName) => {
+
+        if (Array.isArray(items[0][product]) === false) {
+
+            if (Number(items[0][product].price.slice(1)) > 65) {
+
+                productsAbove65[0][product] = { ...items[0][product] };
+
+            }
+
+        } else if (Array.isArray(items[0][product]) === true) {
+
+            let utemsilsName = items[0][product].map((element, index, array) => {
+
+                return Object.keys(items[0][product][index]);
+
+            });
+
+            let utensilsProducts = utemsilsName.reduce((utensilsAbove65, untensil, index, array) => {
+
+                if (Number(items[0][product][index][untensil].price.slice(1)) > 65) {
+
+                    utensilsAbove65[untensil] = { ...items[0][product][index][untensil] };
+
+                }
+
+                return utensilsAbove65;
+
+            }, {});
+
+            productsAbove65[0][product] = [{ ...utensilsProducts }];
+
+        }
+
+        return productsAbove65;
+
+    }, [{}]);
+
+    return desiredProduct;
+
+}
+
+console.log(JSON.stringify(itemsPriceMoreThen$65(products)));
+
 // Question 2.
 
 function quantity(products) {
 
     let productKeys = Object.keys(products[0]);
 
-    let result = productKeys.map((el, ind, arr) => {
+    let result = productKeys.map((element, index, array) => {
 
-        if (typeof (products[0][el]) === 'object' && products[0][el].quantity > 1) {
+        if (typeof (products[0][element]) == 'object' && products[0][element].quantity > 1) {
 
-            return el = products[ind][el];
+            return element = products[0][element];
 
         } else {
 
-            products[0][el].map((e, ind) => {
+            return products[0][element].map((elementInner, indexInner) => {
 
-                if (products[0][el][ind].quantity > 1) {
+                if (products[0][element][indexInner].quantity > 1) {
 
-                    let el = [];
-                    return el.push(products[0][el][ind]);
+                    let element = [];
+
+                    return element.push(products[0][element][indexInner]);
 
                 }
-            })
-        }
 
-        return result;
+            });
+
+        }
 
     });
 
+    return result;
+
 }
 
-console.log(quantity(products));
+// console.log(quantity(products));
 
 // Question 3.
 
 // Question 4.
 
 // Question 5.
+
+// console.log(products);
